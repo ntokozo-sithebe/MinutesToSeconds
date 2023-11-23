60c5c56432aeafefefb7eb1c500c4e550823dbe1
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-23 16:26:25 +0200

    need2finish

---
+++ script/code.js
@@ -0,0 +1,20 @@
+//const input = document.querySelector['data-min']
+let minutes = +document.getElementsByTagName('input').value
+console.log(minutes * 60)
+
+
+function min2Sec(minutes){             
+    
+let seconds = minutes *  60 
+
+return(seconds)
+}
+
+const input = document.querySelector('[data-display]')
+input.addEventListener('input',()=>{
+    
+})
+
+
+
+// ---- {seconds / 60 = <minutes>  |----|  minutes * 60 = <seconds></seconds>}
