5738e34cd209adce51e2ddfbd81a5efb4d10997d
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-24 15:04:35 +0200

    finish@home

--- script/code.js
+++ script/code.js
@@ -1,21 +1,26 @@
 //const input = document.querySelector['data-min']
 
-let minutes = +document.getElementsByTagName('input').value
+let minutes = +document.getElementsByTagName('input')
 console.log(Math.floor(minutes/60))
 
+let results 
+
 function min2Sec(minutes){             
-    
+    let min = minutes.value;
+    // we need to convert age to days lived basically
+    let seconds = min / 60
+
+    results.textContent = daysLived
 //let seconds = (minutes * 60)
 //minutes = (seconds / 60)
-
-return(minutes * 60)
 }
 
 const input = document.querySelector('[data-display]')
-input.addEventListener('input',()=>{ 
+input.addEventListener('click', ()=>{ 
     min2Sec()
     
 })
+
 min2Sec()
 
 
