1e80338a3e15baf5fecfb3fe61677cdbfb9832a5
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-27 14:17:05 +0200

    done

--- script/code.js
+++ script/code.js
@@ -1,6 +1,6 @@
 
 
-
+/*
 let minutes = document.getElementsByTagName('input')
 let results = document.querySelector('[data-display]')
 let btnSubmit = document.getElementById('submit')
@@ -23,4 +23,21 @@ btnSubmit.addEventListener('click', min2Sec);
 
 
 // ---- {seconds / 60 = <minutes>  |----|  minutes * 60 = <seconds></seconds>}
+
+*/
+
+let btnSubmit = document.getElementById('submit')
+
+function min2Sec(){  
+
+    let minutes = +document.querySelector('[data-min]').value
+    let results = document.querySelector('[data-display]')
+    //minutes = minutes.value;
+//let min = parseInt(Input.value);         // takes the input and converts it
+let seconds = minutes * 60;
+results.textContent = `${minutes} minutes is equal to ${seconds} seconds`;
+}
+
+btnSubmit.addEventListener('click', min2Sec);
+
 
